a67cb6fa28e91ef8b027971b3f3a356f3cb858a4
Sebastiaan <sebastiaan@arendsen.dev>
2024-12-12 22:58:09 +0100

    updated app module

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,9 +1,12 @@
 import { Module } from '@nestjs/common';
 import { PromptModule } from '@pursuit/prompt';
+import { AppService } from './app.service';
+import { AppController } from './app.controller';
+import { ArxivModule } from '@pursuit/arxiv';
 
 @Module({
-  imports: [PromptModule],
-  controllers: [],
-  providers: [],
+  imports: [PromptModule, ArxivModule],
+  controllers: [AppController],
+  providers: [AppService],
 })
 export class AppModule {}

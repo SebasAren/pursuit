31d2c74ec4671d0ff4b1c5a9d3dd24a079f23c99
Sebastiaan <sebastiaan@arendsen.dev>
2024-12-12 22:59:38 +0100

    updated app service spec

--- src/app/app.service.spec.ts
+++ src/app/app.service.spec.ts
@@ -1,11 +1,14 @@
 import { Test, TestingModule } from '@nestjs/testing';
 import { AppService } from './app.service';
+import { PromptModule } from '@pursuit/prompt';
+import { ArxivModule } from '@pursuit/arxiv';
 
 describe('AppService', () => {
   let service: AppService;
 
   beforeEach(async () => {
     const module: TestingModule = await Test.createTestingModule({
+      imports: [PromptModule, ArxivModule],
       providers: [AppService],
     }).compile();
 

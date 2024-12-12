db802d9dcfed13b8022c7eded53fd89f6b25cc4b
Sebastiaan <sebastiaan@arendsen.dev>
2024-12-12 22:51:14 +0100

    removed arxiv import from prompt and exported prompt service from prompt module

--- libs/prompt/src/lib/prompt.module.ts
+++ libs/prompt/src/lib/prompt.module.ts
@@ -1,12 +1,10 @@
 import { Module } from '@nestjs/common';
 import { PromptService } from './prompt.service';
 import { PromptController } from './prompt.controller';
-import { ArxivModule } from '@pursuit/arxiv';
 
 @Module({
   controllers: [PromptController],
   providers: [PromptService],
-  exports: [],
-  imports: [ArxivModule],
+  exports: [PromptService],
 })
 export class PromptModule {}

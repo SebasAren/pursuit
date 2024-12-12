7c13ebb747494962edb14e03d78481b60243f76d
Sebastiaan <sebastiaan@arendsen.dev>
2024-12-12 21:45:45 +0100

    removed app services and controller

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,11 +1,9 @@
 import { Module } from '@nestjs/common';
-import { AppController } from './app.controller';
-import { AppService } from './app.service';
 import { ArxivModule } from '@pursuit/arxiv';
 
 @Module({
   imports: [ArxivModule],
-  controllers: [AppController],
-  providers: [AppService],
+  controllers: [],
+  providers: [],
 })
 export class AppModule {}
